7f5f0f2f8c4a1c20db4c73c286829e38562e4aee
Akshat Chaurasia <akshatchaurasia212005@gmail.com>
2026-06-24 23:28:06 +0530

    Relocated index.js | Constants stored in constant.js in src/utils

---
+++ src/utils/constants.js
@@ -0,0 +1,19 @@
+const availableRoles = {
+    ADMIN: "Admin",
+    Project_Admin: "Project_Admin",
+    Member: "Member",
+};
+
+const availableRolesEnum = Object.values(availableRoles);
+
+export default availableRolesEnum;
+
+const availableTaskStatus = {
+    todo: "todo",
+    in_progress: "in_progress",
+    done: "done",
+};
+
+const availableTaskStatusEnum = Object.values(availableTaskStatus);
+
+export default availableTaskStatusEnum;
